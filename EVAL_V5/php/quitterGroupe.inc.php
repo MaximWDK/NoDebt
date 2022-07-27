f8f2e0418736af7348aa5ec66a1e048400c4e066
Maxim_WDK <maxim.leonet@gmail.com>
2022-07-27 23:21:05 +0200

    Ajout de permissions pour supprimer / quitter un groupe

--- EVAL_V5/php/quitterGroupe.inc.php
+++ EVAL_V5/php/quitterGroupe.inc.php
@@ -4,7 +4,13 @@ if (isset($_POST['noSubmit'])) {
 }
 
 if (isset($_POST['submit'])) {
-    $_SESSION['message'] = "<h1>Vous avez quitté le groupe avec succès !</h1>";
-    $pr->removeParticipate($_SESSION['uid'], $gid);
+    $toutGroupe = $gr->getGroupById($gid);
+    if($_SESSION['uid'] == $toutGroupe->uid) {
+        $_SESSION['message'] = "<h1>Si vous souhaitez quitter ce groupe alors que vous en êtes le créateur, vous devez alors passer par le bouton supprimer !</h1>";
+    } else {
+        $_SESSION['message'] = "<h1>Vous avez quitté le groupe avec succès !</h1>";
+        $pr->removeParticipate($_SESSION['uid'], $gid);
+    }
+
 }
 ?>
--- EVAL_V5/php/suppressionGroupe.inc.php
+++ EVAL_V5/php/suppressionGroupe.inc.php
@@ -5,9 +5,10 @@ if (isset($_POST['noSubmit'])) {
 
 if (isset($_POST['submit'])) {
     $groupe = $gr->getGroupById($gid);
-
     if (!$groupe) {
         $_SESSION['message'] = "<h1>Ce groupe a déjà été supprimé !</h1>";
+    } else if ($_SESSION['uid'] != $groupe->uid) {
+        $_SESSION['message'] = "<h1>Seul le créateur du groupe a la permission de le supprimer !</h1>";
     } else {
         $pr->removeAllParticipate($gid);
         $gr->removeGroup($gid);
